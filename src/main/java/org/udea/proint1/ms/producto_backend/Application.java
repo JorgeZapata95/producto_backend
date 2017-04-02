db42e0edb96ad578bb9ccea933bdd24091000593
JorgeZapata95 <jorge_101995@hotmail.com>
2017-04-01 21:17:06 -0500

    correcion dependencias

--- src/main/java/org/udea/proint1/ms/producto_backend/Application.java
+++ src/main/java/org/udea/proint1/ms/producto_backend/Application.java
@@ -1,8 +1,5 @@
 package org.udea.proint1.ms.producto_backend;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
